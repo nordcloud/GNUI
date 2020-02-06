70f41de5f7b04e640f101ad4218745bc6aed9584
Jakub Holak <jakub.holak@nordcloud.com>
2020-02-06 15:38:35 +0100

    test

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { Button } from "@storybook/react/demo";
 
 export default {
-  title: 'Button',
-  component: Button,
+  title: "Button",
+  component: Button
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const Text = () => (
+  <Button onClick={action("clicked")}>Hello Button</Button>
+);
 
 export const Emoji = () => (
-  <Button onClick={action('clicked')}>
+  <Button onClick={action("clicked nice")}>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
