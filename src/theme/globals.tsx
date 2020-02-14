69d1ff0ef16942618ff2736fde21b3b712c01fbd
Robert Grzonka <robert.grzonka@nordcloud.com>
2020-02-14 11:20:29 +0100

    Improve global styles and theme design

--- src/theme/globals.tsx
+++ src/theme/globals.tsx
@@ -1,15 +1,24 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import theme from "./index";
 
 export const SetGlobalStyle = createGlobalStyle`
   ${reset}
   @import url('https://fonts.googleapis.com/css?family=Montserrat:400,500&display=swap');
-  @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,600,700&display=swap');
   body {
-     font-family: 'Nunito Sans', sans-serif;
-     background: #d8d8d8;
+     font-family: 'Roboto', sans-serif;
+     background: ${theme.colors.light};
+     font-weight: ${theme.fontWeights.regular};
   }
   h1, h2, h3, h4, h5, h6 {
       font-family:"Montserrat", sans-serif;
+      color: ${theme.colors.primary}
+  }
+  h1, h2 {
+    font-weight: ${theme.fontWeights.medium};
+  }
+  h3, h4, h5, h6 {
+    font-weight: ${theme.fontWeights.bold};
   }
 `;
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,19 +1,43 @@
 interface Colors {
-    primary: string;
-    light: string;
-    dark: string;
+  [propName: string]: string;
+}
+
+interface FontWeights {
+  [propName: string]: number;
 }
 
 export interface ThemeInterface {
-    colors: Colors;
+  colors: Colors;
+  fontWeights: FontWeights;
 }
 
 const theme: ThemeInterface = {
-    colors: {
-        primary: '#2e2e47',
-        light: '#ffffff',
-        dark: '#333333',
-    },
+  colors: {
+    primary: "#161632",
+    accent: "#FF2837",
+    danger: "#e74c3c",
+    success: "#27ae60",
+    warning: "#f39c12",
+    notification: "#3498db",
+    dark: "#212129",
+    dark500: "#212129",
+    dark400: "#32323f",
+    dark300: "#444454",
+    dark200: "#5e5e75",
+    dark100: "#7a7a94",
+    light500: "#bbbbc9",
+    light400: "#d1d1da",
+    light300: "#e7e7eb",
+    light200: "#f1f1f4",
+    light100: "#fcfcfd",
+    light: "#fcfcfd"
+  },
+  fontWeights: {
+    regular: 400,
+    medium: 500,
+    bold: 600,
+    bolder: 700
+  }
 };
 
 export default theme;
