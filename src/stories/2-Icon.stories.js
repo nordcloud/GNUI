04e9a5c36a6e9766fb4b6d5006768a6b037b982b
Jakub Holak <jakub.holak@nordcloud.com>
2020-02-14 16:23:44 +0100

    Add cloud providers icons

--- src/stories/2-Icon.stories.js
+++ src/stories/2-Icon.stories.js
@@ -1,20 +1,29 @@
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-import { wInfo } from '../utils/wInfo';
-import { Icon } from '../components/icon';
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import { wInfo } from "../utils/wInfo";
+import { Icon } from "../components/icon";
 
-const stories = storiesOf('Components/Icon', module);
+const stories = storiesOf("Components/Icon", module);
 
 stories.add(
-    'status - enabled',
-    wInfo()(() => (
-        <Icon width="24px" height="22px" image="STATUS_ENABLED"></Icon>
-    )),
+  "status - enabled",
+  wInfo()(() => <Icon width="24px" height="22px" image="STATUS_ENABLED"></Icon>)
 );
 
 stories.add(
-    'status - disabled',
-    wInfo()(() => (
-        <Icon width="24px" height="22px" image="STATUS_DISABLED"></Icon>
-    )),
+  "status - disabled",
+  wInfo()(() => (
+    <Icon width="24px" height="22px" image="STATUS_DISABLED"></Icon>
+  ))
+);
+
+stories.add(
+  "cloud providers",
+  wInfo()(() => (
+    <>
+      <Icon width="24px" height="24px" image="GCP"></Icon>
+      <Icon width="24px" height="24px" image="AWS"></Icon>
+      <Icon width="24px" height="24px" image="AZURE"></Icon>
+    </>
+  ))
 );
