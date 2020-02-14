85c4ca23a5063b1fd2997b47181238c2fed3ec48
Agnieszka Leszczyńska <agnieszka.leszczynska@nordcloud.com>
2020-02-14 09:02:26 +0100

    Add Icon component to GNUI

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import { icons } from "../../utils/icons";
+
+const svgToMiniDataURI = require('mini-svg-data-uri');
+
+interface IconProps {
+  image: string;
+  width: string;
+  height: string;
+  inline: boolean;
+}
+
+const StyledIcon = styled.div<IconProps>`
+  display: ${props => (props.inline ? "inline-block" : "ś")};
+  width: ${props => props.width};
+  height: ${props => props.height};
+  background-image: url("${props => svgToMiniDataURI(icons[props.image])}");
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+export const Icon: React.FC<IconProps> = props => {
+  return <StyledIcon {...props}></StyledIcon>;
+};
