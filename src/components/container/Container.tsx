213a10a7f77da605952b639460760a77be2d70ff
Robert Grzonka <robert.grzonka@nordcloud.com>
2020-02-20 14:06:55 +0100

    Add arrows icons

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 import theme from "../../theme";
 
 export const Container = styled.div`
+  box-sizing: border-box;
+  width: 100%;
   font-family: ${theme.fontFamily.regular};
   font-weight: ${theme.fontWeights.regular};
   font-size: 1rem;
   line-height: 1.5rem;
-  width: 100%;
 `;
 
 export const Flex = styled(Container)`
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,23 +1,11 @@
 import * as React from "react";
 import styled from "styled-components";
-import theme from "../../theme";
+import { Flex } from "../container";
 
-interface FormProps {
-  className: string;
-  width?: string;
-}
-
-const StyledForm = styled.form<FormProps>`
-  display: flex;
+const StyledForm = styled(Flex)`
   flex-wrap: wrap;
-  justify-content: space-between;
-  font-family: ${theme.fontFamily.regular};
-  font-weight: ${theme.fontWeights.regular};
-  line-height: 1.5rem;
-  font-size: 1rem;
-  width: ${props => (props.width ? props.width : "100%")};
 `;
 
-export const Form: React.FC<FormProps> = props => (
+export const Form: React.FC = props => (
   <StyledForm {...props}>{props.children}</StyledForm>
 );
