e33ce64a3aeade96577752dd90c4f958c4cb4274
Robert Grzonka <robert.grzonka@nordcloud.com>
2020-04-07 13:52:51 +0200

    Add few Flexbox features (i.e. breakpoints)

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,12 +1,6 @@
 import React, { FunctionComponent } from "react";
 import styled, { css } from "styled-components";
-import { createBreakpoint, createMap } from "styled-components-breakpoint";
-import theme from "../../theme";
-
-const { breakpoints } = theme;
-
-const bp = createBreakpoint(breakpoints);
-const map = createMap(breakpoints);
+import theme, { margin, padding, spacings, map } from "../../theme";
 
 interface FlexProps {
   children?: any;
@@ -44,24 +38,32 @@ interface FlexProps {
   flex?: string;
 }
 
+interface ColumnProps {
+  size: number;
+  m: number;
+  p: number;
+}
+
 const basicFlexStyles = (props: { color?: string }) => css`
   color: ${props.color || theme.colors.primary};
   letter-spacing: 0.0525rem;
+  & > * {
+    color: inherit;
+  }
 `;
 
 const FlexItem = styled.div<FlexProps>`
   ${basicFlexStyles};
-  ${props =>
+  ${(props) =>
     props &&
     css`
       display: ${props.display || "flex"};
-      justify-content: ${props.justifyContent || "space-between"};
+      justify-content: ${props.justifyContent || "space-around"};
       flex-direction: ${props.flexDirection || "row"};
       flex-grow: ${props.flexGrow || 0};
       flex-basis: ${props.flexBasis || "auto"};
       flex-shrink: ${props.flexShrink || 1};
       flex-wrap: ${props.flexWrap || "wrap"};
-      flex: ${props.flex || "0 1 auto"};
       align-items: ${props.alignItems || "stretch"};
       align-content: ${props.alignContent || "center"};
     `}
@@ -71,26 +73,76 @@ export const Grid: FunctionComponent = ({ children, ...props }) => (
   <FlexItem {...props}>{children}</FlexItem>
 );
 
-export const Row = styled(FlexItem)<{ cols: number }>`
+const checkMargins = (marginPropValue: number | string | any): any => {
+  const cache = (value: any, rem: boolean = true): any =>
+    css`
+      margin-left: ${rem ? `${value}rem` : `${value}`};
+      margin-right: ${rem ? `${value}rem` : `${value}`};
+    `;
+
+  if (typeof marginPropValue === "number") {
+    return cache(marginPropValue, true);
+  }
+
+  for (const obj of spacings) {
+    const [key, value] = Object.entries(obj).flat();
+    if (key === marginPropValue) {
+      return cache(value, true);
+    }
+  }
+
+  return cache(marginPropValue, false);
+};
+
+export const Row = styled(FlexItem)<{ m: number }>`
   flex-basis: 100%;
+  max-width: 100%;
+  & > * {
+    overflow: hidden;
+  }
+
+  ${({ m }) => m && checkMargins(m)}
 `;
 
-const flexBasisQuery = (size: number): any =>
+const flexBasisQuery = (size: number, m = margin): any =>
   map(
     size,
-    val =>
-      `flex-basis: calc(8.3% * ${val} - 0.2rem); max-width: calc(8.3% * ${val} - 0.2rem)`
+    (val) =>
+      `flex-basis: calc(8.3% * ${val} - ${2 * m}rem);
+      max-width: calc(8.3% * ${val} - ${2 * m}rem)`
   );
 
-export const Column = styled.div<{ size: number; order: number }>`
-  margin: 0.1rem;
+const setColumnMargin = (size: number, m: any) => {
+  if (typeof size === "number") {
+    return css`
+      flex-basis: calc(8.3% * ${size} - 2 * ${m ? m : margin}rem);
+      max-width: calc(8.3% * ${size} - 2 * ${m ? m : margin}rem);
+    `;
+  }
+
+  if (typeof size !== "undefined" && typeof size !== "number") {
+    return flexBasisQuery(size, m);
+  }
+
+  return css`
+    flex-basis: auto;
+    max-width: calc(100% - 2 * ${m ? m : margin}rem);
+  `;
+};
+
+export const Column = styled.div<ColumnProps>`
+  box-sizing: border-box;
   flex-grow: 1;
+  padding-left: ${padding}rem;
+  padding-right: ${padding}rem;
+  margin: ${margin}rem;
 
-  ${({ size }) => flexBasisQuery(size)};
-
-  ${({ order }) =>
-    order &&
+  ${({ p }) =>
+    p &&
     css`
-      order: ${order};
+      padding-left: ${p}rem;
+      padding-right: ${p}rem;
     `}
+
+  ${({ size, m }) => ((size && m) || size) && setColumnMargin(size, m)}
 `;
