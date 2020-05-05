b178fc73d19dfdda7450d0a0c4775c53560d8493
daniel-choinski <daniel.choinski@nordcloud.com>
2020-05-05 09:46:13 +0200

    Remove unused imports

--- src/components/toggle/Toggle.tsx
+++ src/components/toggle/Toggle.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent } from "react";
 import styled, { css } from "styled-components";
 import theme from "../../theme";
 import { darken } from "polished";
