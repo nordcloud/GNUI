686d232750d6aa0f6da2b0383d207f433fec8731
daniel-choinski <daniel.choinski@nordcloud.com>
2020-05-05 09:42:00 +0200

    Add navigation pageChange function instead of using a tag

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -5,16 +5,16 @@ import theme from "../../theme";
 export interface NavLinkProps {
   name: string;
   link: string;
-  id?: string;
+  onClick: () => void;
   className?: string;
   value?: string;
-  onClick?: (e: React.MouseEvent) => void;
 }
 
 export interface NavProps {
   name?: string;
   names: Array<string>;
   links: Array<string>;
+  onClick: (link: string) => void;
   children?: string | number | any;
 }
 
@@ -24,22 +24,21 @@ export interface NavWrapperProps {
 
 const MenuLink = styled.li`
   list-style: none;
-  a {
-    line-height: ${theme.lineHeight};
-    font-family: ${theme.fonts.body};
-    font-size: ${theme.fontSizes.regular};
-    border-radius: ${theme.radius.default};
-    padding: ${theme.spacing.spacing02} ${theme.spacing.spacing03};
-    color: ${theme.colors.primary};
-    text-decoration: none;
+  display: inline-block;
+  line-height: ${theme.lineHeight};
+  font-family: ${theme.fonts.body};
+  font-size: ${theme.fontSizes.regular};
+  border-radius: ${theme.radius.default};
+  padding: ${theme.spacing.spacing02} ${theme.spacing.spacing03};
+  color: ${theme.colors.primary};
+  text-decoration: none;
 
-    &:hover {
-      background-color: ${theme.colors.lights[2]};
-    }
-    &:active,
-    &:focus {
-      background-color: ${theme.colors.lights[3]};
-    }
+  &:hover {
+    background-color: ${theme.colors.lights[2]};
+  }
+  &:active,
+  &:focus {
+    background-color: ${theme.colors.lights[3]};
   }
 `;
 
@@ -61,9 +60,7 @@ const NavWrapper = styled.ul<NavWrapperProps>`
       display: block;
       li {
         margin: 0 0 ${theme.spacing.spacing02};
-        a {
-          display: block;
-        }
+        display: block;
       }
     `}
 `;
@@ -73,22 +70,21 @@ export const NavLink: FunctionComponent<NavLinkProps> = ({
   link,
   ...props
 }) => (
-  <MenuLink>
-    <a href={link} title={name} {...props}>
-      {name}
-    </a>
+  <MenuLink id={link} {...props}>
+    {name}
   </MenuLink>
 );
 
 export const Navigation: FunctionComponent<NavProps> = ({
   names,
   links,
+  onClick,
   ...props
 }) => (
   <NavWrapper {...props}>
     {names &&
       names.map((element, index) => (
-        <NavLink key={index} name={element} link={links[index]} />
+        <NavLink key={index} name={element} link={links[index]} onClick={() => onClick(links[index])}/>
       ))}
   </NavWrapper>
 );
