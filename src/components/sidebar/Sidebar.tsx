a7961f0e8327ec1d8fcef25fcdba2422efe812af
Robert Grzonka <robert.grzonka@nordcloud.com>
2020-05-05 11:11:06 +0200

    Fix minor bugs
    * change z-index hardcoded alues to `theme.zindex`
    * change line-height hardcoded values to `theme.lineHeight`
    * change transition

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -12,33 +12,33 @@ const SidebarMenu = styled(Container)`
   border-radius: 0;
   width: 0;
   height: 100vh;
-  z-index: 100;
+  z-index: ${theme.zindex.masked};
   position: absolute;
   border-left: ${theme.borders.disabled};
   background-color: ${theme.colors.white};
   box-shadow: ${theme.shadow.shadow04};
-  transition: all 0.4s ease-in-out;
+  transition: width 0.3s ease-in-out;
 `;
 
 const SidebarButton = styled(Button)<any>`
   position: fixed;
   top: 1rem;
   right: 1rem;
-  z-index: 100;
+  z-index: ${theme.zindex.masked};
 `;
 
 const SidebarTitle = styled(Text)`
-  line-height: 1.5rem;
+  line-height: ${theme.lineHeight};
   font-size: ${theme.fontSizes.medium};
   font-weight: ${theme.fontWeights.bold};
   padding: ${theme.spacing.spacing06};
-  margin: 0 auto;
   border-bottom: ${theme.borders.grey};
+  margin: 0 auto;
 `;
 
 const SidebarContent = styled(Text)`
   font-weight: ${theme.fontWeights[0]};
-  line-height: 1.5rem;
+  line-height: ${theme.lineHeight};
   font-size: ${theme.fontSizes.medium};
   font-weight: ${theme.fontWeights.regular};
   padding: ${theme.spacing.spacing06};
@@ -48,6 +48,7 @@ const SidebarContent = styled(Text)`
 const Wrapper: any = styled(Container)<{ isOpen: boolean }>`
   & ${SidebarMenu} {
     width: 0;
+    transition: width 0.3s ease-in-out;
   }
 
   ${({ isOpen }) =>
@@ -56,36 +57,38 @@ const Wrapper: any = styled(Container)<{ isOpen: boolean }>`
       bottom: 0;
       right: 0;
       position: fixed;
-      z-index: 1;
+      z-index: ${theme.zindex.default};
       width: 100vw;
       height: 100vh;
       overflow-x: hidden;
       overflow-y: scroll;
       background-color: rgba(0, 0, 0, 0.6);
-      transition: all 0.4s ease-in-out;
+      transition: background-color 0.3s ease-in-out;
 
       & ${SidebarMenu} {
         width: 100%;
         ${bp("sm")`width: 50%`};
-        ${bp("md")`width: 40%`}
+        ${bp("md")`width: 40%`};
+        transition: width 0.3s ease-in-out;
       }
 
       & ${SidebarButton} {
         position: absolute;
-        right: 0.5rem;
-        transition: all 0.4s ease-in-out;
       }
     `}
+  transition: background-color 0.3s ease-in-out;
 `;
 
 interface SidebarProps {
   children?: React.ReactNode;
   title?: string;
+  onChange: (e: any) => void;
 }
 
 const Sidebar: FunctionComponent<SidebarProps> = ({
   children,
   title,
+  onChange,
   ...props
 }) => {
   const [isOpen, setIsOpen] = useState(false);
