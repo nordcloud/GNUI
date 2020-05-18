43ad5b9e53dd7652d5d4ff5a1728538c34a31825
Elena Kazakova <jphawk@Elenas-MacBook-Pro.local>
2020-05-18 14:46:39 +0300

    Add Spinner

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import theme from "../../theme";
+import { Text } from "../text";
 
 export interface SpinnerProps {
-  labelText?: string;
+  caption?: string;
   className?: string;
+  size?: number;
   color?: string;
   onClick?: (e: any) => void;
   onChange?: (e: React.FormEvent<HTMLInputElement>) => void;
@@ -16,102 +18,68 @@ const setColor = (color: string) => {
     : color;
 };
 
-const StyledSpinner = styled.div`
+const SpinnerContainer = styled.div<SpinnerProps>`
+  display:flex;
+  align-items:center;
+  p {
+    margin: 0 0 0 ${({ caption }) =>
+    caption ?  theme.spacing.spacing02 : "0"};
+  }
+`;
+
+const StyledRing = styled.div<SpinnerProps>`
   display: inline-block;
   position: relative;
   width: 5rem;
   height: 5rem;
+  zoom: ${({ size }) =>
+  size ? `${size}` : `1`};
 
   div {
-    animation: spinner 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    transform-origin: 40px 40px;
-
-    &:after {
-      content: " ";
-      display: block;
-      position: absolute;
-      width: 7px;
-      height: 7px;
-      border-radius: 50%;
-      background: ${({ color }) =>
-      color ? `${setColor(color)}` : `${theme.colors.primary}`};;
-      margin: -4px 0 0 -4px;
-    }
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 4rem;
+    height: 4rem;
+    margin: 0.5rem;
+    border: 0.5rem solid ${({ color }) =>
+    color ? `${setColor(color)}` : `${theme.colors.primary}`};
+    border-radius: 50%;
+    animation: ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: ${({ color }) =>
+    color ? `${setColor(color)}` : `${theme.colors.primary}`} transparent transparent transparent;
     &:nth-child(1) {
-      animation-delay: -0.036s;
-      &:after {
-        top: 63px;
-        left: 63px;
-      }
+      animation-delay: -0.45s;
     }
     &:nth-child(2) {
-      animation-delay: -0.072s;
-      &:after {
-        top: 68px;
-        left: 56px;
-      }
+      animation-delay: -0.3s;
     }
     &:nth-child(3) {
-      animation-delay: -0.108s;
-      &:after {
-        top: 71px;
-        left: 48px;
-      }
-    }
-    &:nth-child(4) {
-      animation-delay: -0.144s;
-      &:after {
-        top: 72px;
-        left: 40px;
-      }
-    }
-    &:nth-child(5) {
-      animation-delay: -0.18s;
-      &:after {
-        top: 71px;
-        left: 32px;
-      }
-    }
-    &:nth-child(6) {
-      animation-delay: -0.216s;
-      &:after {
-        top: 68px;
-        left: 24px;
-      }
-    }
-    &:nth-child(7) {
-      animation-delay: -0.252s;
-      &:after {
-        top: 63px;
-        left: 17px;
-      }
-    }
-    &:nth-child(8) {
-      animation-delay: -0.288s;
-      &:after {
-        top: 56px;
-        left: 12px;
-      }
+      animation-delay: -0.15s;
     }
   }
 
-@keyframes spinner {
-  0% {
-    transform: rotate(0deg);
+  @keyframes ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
-  100% {
-    transform: rotate(360deg);
-  }
-}
 `;
 
 export const Spinner: FunctionComponent<SpinnerProps> = ({
-  labelText
+  caption,
+  color,
+  size
 }) => {
   return (
-    <StyledSpinner>
-      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-      {labelText}
-    </StyledSpinner>
+    <SpinnerContainer caption={caption}>
+      <StyledRing color={color} size={size}>
+        <div></div><div></div><div></div><div></div>
+      </StyledRing>
+      {caption && <Text>{caption}</Text>}
+    </SpinnerContainer>
   );
 };
