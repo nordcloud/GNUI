e21463a1566312c7a7897af5e5bf72dddb89fa83
jphawk <eo_kazakova@yahoo.com>
2020-06-02 14:37:48 +0300

    Add Datepicker (#101)
    * Add Datepicker
    
    * Solve merge conflict
    
    Co-authored-by: Elena Kazakova <jphawk@Elenas-MacBook-Pro.local>

---
+++ src/components/datepicker/Datepicker.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import theme from "../../theme";
+
+
+type DatepickerProps = {
+  children?:any;
+};
+
+const DatepickerWrapper = styled.div` 
+  .rdrCalendarWrapper,
+  .rdrDateRangePickerWrapper {
+    color: ${theme.colors.primary};
+    font-family: ${theme.fonts.body};
+    font-weight: ${theme.fontWeights.regular};
+    font-size: ${theme.fontSizes.small};
+    box-shadow:${theme.shadow.shadow04};
+    border-radius:${theme.radiusDefault};
+  }
+  .rdrDateRangePickerWrapper {
+    .rdrCalendarWrapper {
+      box-shadow:none;
+    }
+  }
+`;
+
+export const Datepicker: FunctionComponent<DatepickerProps> = ({
+  children
+}) => (
+    <DatepickerWrapper>
+      {children}
+    </DatepickerWrapper>
+    )
+;
