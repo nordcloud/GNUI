4114e1f5ffd45d55baf45c8d281d889d4518a5e9
jphawk <eo_kazakova@yahoo.com>
2020-06-10 11:27:23 +0300

    Multiselect props and Plus icon (#110)
    Co-authored-by: Elena Kazakova <jphawk@Elenas-MacBook-Pro.local>

--- src/components/multiselect/Multiselect.tsx
+++ src/components/multiselect/Multiselect.tsx
@@ -1,7 +1,8 @@
 import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import theme from "../../theme";
-import {default as ReactSelect} from 'react-select';
+import {default as ReactSelect, CommonProps } from 'react-select';
+
 
 export interface SelectProps {
   options?:any;
@@ -84,7 +85,7 @@ const SelectContainer = styled.div<SelectProps>`
   }
 `;
 
-export const Select: FunctionComponent<SelectProps> = ({
+export const Select: FunctionComponent<SelectProps & CommonProps<any>> = ({
   options,
   styles,
   ...props
