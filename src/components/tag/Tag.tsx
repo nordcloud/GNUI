84ef6273279d4b86c84ae455370e8b85482445d2
Jakub Holak <jakub.holak@nordcloud.com>
2020-07-01 22:07:06 +0200

    Extend tag options to make it clickable

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -5,6 +5,8 @@ import theme from "../../theme";
 interface TagProps {
   color?: string;
   text?: string;
+  onClick?: () => void;
+  showClose?: boolean;
 }
 
 export const StyledTag = styled.div`
@@ -20,8 +22,35 @@ export const StyledTag = styled.div`
   color: ${theme.colors.lights[0]};
   background-color: ${({ color }) =>
     color ? theme.colors[color] || color : theme.colors.lights[3]};
+  cursor: ${({ onClick }) => onClick && "pointer"};
+  span {
+    width: 0.8rem;
+    height: 0.8rem;
+    border-radius: 1rem;
+    background: rgba(255, 255, 255, 0.3);
+    display: block;
+    float: right;
+    font-size: 0.5rem;
+    line-height: 0.82rem;
+    font-weight: bold;
+    margin: 0.2rem 0 0 0.4rem;
+    transform: translate(2px, 1px);
+  }
+  &:hover {
+    opacity: ${({ onClick }) => onClick && "0.8"};
+  }
 `;
 
-export const Tag: React.FC<TagProps> = ({ color, text }) => {
-  return <StyledTag color={color}>{text || "No data"}</StyledTag>;
+export const Tag: React.FC<TagProps> = ({
+  color,
+  text,
+  onClick,
+  showClose,
+}) => {
+  return (
+    <StyledTag color={color} onClick={onClick}>
+      {text || "No data"}
+      {showClose && <span>x</span>}
+    </StyledTag>
+  );
 };
