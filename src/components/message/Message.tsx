a1dcdf9a34eb82a359a7feef8bb2c44ee5687a0f
kuba holak <jakub.holak@nordcloud.com>
2020-07-06 15:18:24 +0200

    Add MessageBox customization options

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -6,33 +6,46 @@ import { Icon } from "../icon";
 export interface MessageProps {
   image?: string;
   status?: "success" | "notification" | "danger";
+  borderColor?: string;
+  background?: string;
+  color?: string;
   children?: React.ReactNode;
-};
+}
 
 export const MessageWrapper = styled.div<MessageProps>`
-  display:flex;
-  align-items:center;
+  display: flex;
+  align-items: center;
   border-radius: ${theme.radiusDefault};
   color: ${theme.colors.white};
   font-size: ${theme.fontSizes.regular};
   padding: ${theme.spacing.spacing03};
-  line-height:1.5rem;
-  
+  line-height: 1.5rem;
+  border:1px solid;
+
   ${({ status }) =>
-  status === "success" &&
-  css`
-    background: ${theme.colors.success};
-  `}
-  ${({ status }) =>
-  status === "danger" &&
-  css`
-    background: ${theme.colors.danger};
-  `}
-  ${({ status }) =>
-  status === "notification" &&
-  css`
-    background: ${theme.colors.notification};
-  `}
+    status &&
+    css`
+      background: ${theme.colors[status]};
+      border-color: ${theme.colors[status]};
+    `}
+
+  ${({ borderColor }) =>
+    borderColor &&
+    css`
+      border: 1px solid ${theme.colors[borderColor] || borderColor};
+    `}
+
+  ${({ background }) =>
+    background &&
+    css`
+      background: ${theme.colors[background] || background};
+    `}
+
+  ${({ color }) =>
+    color &&
+    css`
+      color: ${theme.colors[color] || color};
+    `}
 `;
 
 export const IconBox = styled.div<MessageProps>`
@@ -43,37 +56,41 @@ export const IconBox = styled.div<MessageProps>`
   margin-right: ${theme.spacing.spacing03};
 
   ${({ status }) =>
-  status === "success" &&
-  css`
-    background: rgba(30, 132, 73, 0.5);
-  `}
+    status === "success" &&
+    css`
+      background: rgba(30, 132, 73, 0.5);
+    `}
   ${({ status }) =>
-  status === "danger" &&
-  css`
-    background: rgba(176, 58, 46, 0.5);
-  `}
+    status === "danger" &&
+    css`
+      background: rgba(176, 58, 46, 0.5);
+    `}
   ${({ status }) =>
-  status === "notification" &&
-  css`
-    background: rgba(40, 116, 166, 0.5);
-  `}
+    status === "notification" &&
+    css`
+      background: rgba(40, 116, 166, 0.5);
+    `}
 `;
 
 export const Align = styled.div`
-  margin-bottom:auto;
+  margin-bottom: auto;
 `;
 
 export const Message: FunctionComponent<MessageProps> = ({
   children,
   image,
-  status
-}) => 
-    <MessageWrapper status={status}>
-      {image && 
+  ...props
+}) => (
+  <MessageWrapper {...props}>
+    {image && (
       <Align>
-        <IconBox status={status}>
-          <Icon image={image} width="1.5rem" height="1.5rem"/>
-        </IconBox>
-      </Align>}
-      {children}
-    </MessageWrapper>;
+        {image && (
+          <IconBox {...props}>
+            <Icon image={image} width="1.5rem" height="1.5rem" />
+          </IconBox>
+        )}
+      </Align>
+    )}
+    {children}
+  </MessageWrapper>
+);
