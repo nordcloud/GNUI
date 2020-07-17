97d33fbf3cf697fe4965e9c48bf92106bdd0eb19
Developer <krzysztof.kedziorski@e-qsg.com>
2020-07-17 10:59:32 +0200

    fix pull request comments

--- src/components/svgicon/SVGIcon.tsx
+++ src/components/svgicon/SVGIcon.tsx
@@ -8,13 +8,13 @@ export interface SVGIconProps {
   name: string;
   color?: "success" | "danger" | "warning" | "notification" | "white";
   size?: "sm" | "md" | "lg" | "xl" | "xxl";
+  [x: string]: any;
 }
 
 const StyledSVGIcon = styled.svg<SVGIconProps>`
   fill: ${theme.colors.primary};
   width: ${theme.iconSize.md};
   height: ${theme.iconSize.md};
-  [x: string]: any;
 
   ${({ color }) =>
     color &&
