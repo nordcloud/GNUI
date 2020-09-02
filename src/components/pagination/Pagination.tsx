18bab61e61051393054a51833ef3435dc46c7974
Nazar Horay <nazar.horay@nordcloud.com>
2020-09-02 09:30:33 +0200

    hover effect

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import theme from "../../theme";
 import { Flex } from "../container";
-import {SVGIcon} from "../svgicon";
+import { SVGIcon } from "../svgicon";
 
 interface IPaginationProps {
   set(start: number): void;
@@ -84,15 +84,17 @@ const PerPage = ({ size, set }: IPerPageProps) => (
     aria-label="pagination"
   >
     <div className="pagination-show">Show:</div>
-      {[10, 20, 50].map((el, i) => (
-        <button
-          key={el}
-          onClick={() => set(el)}
-          className={`pagination-link ${el.toString() === size.toString() && `current`}`}
-        >
-          <span>{el}</span>
-        </button>
-      ))}
+    {[10, 20, 50].map((el, i) => (
+      <button
+        key={el}
+        onClick={() => set(el)}
+        className={`pagination-link ${
+          el.toString() === size.toString() && `current`
+        }`}
+      >
+        <span>{el}</span>
+      </button>
+    ))}
   </nav>
 );
 
@@ -108,8 +110,8 @@ const PaginationAmount = ({ from, size, count }: IPaginationAmountProps) => {
   const minCurrentPage = count < s ? "1" : from + 1;
   return (
     <div className="pagination-result">
-      {minCurrentPage} -
-      {maxCurrentPage > count ? count : maxCurrentPage} of {count}
+      {minCurrentPage} -{maxCurrentPage > count ? count : maxCurrentPage} of{" "}
+      {count}
     </div>
   );
 };
@@ -126,15 +128,16 @@ const StyledPaginationBox = styled.nav`
   display: flex;
   width: 100%;
   background-color: ${theme.colors.lights[0]};
-  padding:${theme.spacing.spacing03};
+  padding: ${theme.spacing.spacing03};
   border-top: 2px solid ${theme.colors.lights[2]};
   justify-content: space-between;
   align-items: center;
-  .pagination-result, .pagination-show  {
+  .pagination-result,
+  .pagination-show {
     font-weight: ${theme.fontWeights.regular};
     font-size: ${theme.fontSizes.md};
     color: ${theme.colors.darks[4]};
-    margin:0 .5rem 0 0;
+    margin: 0 0.5rem 0 0;
     line-height: 1.5em;
   }
   .pagination-result {
@@ -148,13 +151,13 @@ const StyledPaginationBox = styled.nav`
     display: flex;
     list-style: none;
     padding-left: 0;
-    margin:0;
+    margin: 0;
   }
   button {
-    display:flex;
+    display: flex;
     align-items: center;
     background: transparent;
-    border:0;
+    border: 0;
     font-weight: ${theme.fontWeights.regular};
     font-size: ${theme.fontSizes.md};
     font-family: ${theme.typography.fonts.body};
@@ -162,53 +165,53 @@ const StyledPaginationBox = styled.nav`
     line-height: 1.5em;
     padding: 0.5rem;
     border-radius: ${theme.radius.lg};
-    margin:0 ${theme.spacing.spacing01};
+    margin: 0 ${theme.spacing.spacing01};
     transition: ${theme.transition};
     background-color: ${theme.colors.lights[1]};
-    
+
     &:hover {
       cursor: pointer;
-      background-color: ${theme.colors.lights[1]};
+      background-color: ${theme.colors.lights[2]};
     }
-    
+
     &:active {
       background-color: ${theme.colors.lights[3]};
     }
-    
+
     &:focus {
       outline: 0;
     }
-    
+
     &.current {
       background-color: ${theme.colors.primary};
-      color:#fff;
+      color: #fff;
       font-weight: ${theme.fontWeights.medium};
     }
-    
+
     &:disabled {
       opacity: 0.5;
       cursor: not-allowed;
-      
+
       &:hover {
         background-color: transparent;
       }
     }
-    
+
     &.pagination-first {
       background-color: transparent;
       svg {
-        padding-left:0;  
+        padding-left: 0;
         margin-right: 0.5rem;
       }
     }
     &.pagination-last {
       background-color: transparent;
       svg {
-        padding-right:0;
+        padding-right: 0;
         margin-left: 0.5rem;
       }
     }
-    
+
     svg {
       width: 1.25rem;
       height: 1.25rem;
@@ -216,7 +219,7 @@ const StyledPaginationBox = styled.nav`
     }
     span {
       min-width: 1.5rem;
-      text-align:center;
+      text-align: center;
     }
   }
 `;
