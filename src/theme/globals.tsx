d8accdbdffaaf6c4287027e0f868d40a29336a3a
raczyk <pawel.raczyk@nordcloud.com>
2020-09-16 16:48:13 +0200

    add global styles

--- src/theme/globals.tsx
+++ src/theme/globals.tsx
@@ -1,9 +1,24 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import theme from ".";
 
 export const SetGlobalStyle = createGlobalStyle`
   ${reset}
   @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap');
+  html {
+    font-size: 16px;
+  }
+  html, body {
+     font-family: ${theme.typography.fonts.body};
+     background: ${theme.colors.lights[2]};
+     color: ${theme.colors.primary};
+     font-weight: ${theme.fontWeights.regular};
+     line-height: ${theme.lineHeight};
+  }
+  code {
+    font-family: ${theme.typography.fonts.body};
+    font-weight: ${theme.fontWeights.regular};
+  }
   a, a:visited {
     text-decoration: none !important;
   }
