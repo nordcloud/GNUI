869148365fc8f3e456c7c05ab3106fb52cb77f2f
raczyk <pawel.raczyk@nordcloud.com>
2020-10-09 13:09:21 +0200

    pagination bugfix

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -21,58 +21,56 @@ const Pagination: React.FC<IPaginationProps> = ({
   const currentPage = Math.ceil(current / size);
   const nPages = Math.ceil(count / size);
   return (
-    <>
-      <nav className="pagination" role="navigation" aria-label="pagination">
-        <ul className="pagination-list">
-          <li>
-            <button
-              onClick={() => set(0)}
-              onKeyDown={() => set(0)}
-              type="button"
-              className="pagination-first"
-              disabled={current <= 0}
-            >
-              <SVGIcon name="chevronLeft" /> <span>First</span>
-            </button>
-          </li>
+    <nav className="pagination" role="navigation" aria-label="pagination">
+      <ul className="pagination-list">
+        <li>
+          <button
+            onClick={() => set(0)}
+            onKeyDown={() => set(0)}
+            type="button"
+            className="pagination-first"
+            disabled={current <= 0}
+          >
+            <SVGIcon name="chevronLeft" /> <span>First</span>
+          </button>
+        </li>
 
-          {[...Array(Math.min(Math.ceil(count / size), 5))]
-            .map((_, i) => currentPage + i)
-            .map((i) =>
-              currentPage === nPages ? i - 4 : current > 3 ? i - 1 : i
-            )
-            .filter((i) => i < nPages)
-            .map((i) => {
-              return (
-                <li key={`p${i}`}>
-                  <button
-                    onClick={() => set(size * i)}
-                    onKeyDown={() => set(size * i)}
-                    type="button"
-                    disabled={i < 0}
-                    className={`pagination-link ${
-                      i === currentPage && `current`
-                    }`}
-                  >
-                    <span>{i < 0 ? "1" : i + 1}</span>
-                  </button>
-                </li>
-              );
-            })}
-          <li>
-            <button
-              onClick={() => set((nPages - 1) * size)}
-              onKeyDown={() => set((nPages - 1) * size)}
-              type="button"
-              className="pagination-last"
-              disabled={current <= 0}
-            >
-              <span>Last</span> <SVGIcon name="chevronRight" />
-            </button>
-          </li>
-        </ul>
-      </nav>
-    </>
+        {[...Array(Math.min(Math.ceil(count / size), 5))]
+          .map((_, i) => currentPage + i)
+          .map((i) =>
+            currentPage === nPages ? i - 4 : current > 3 ? i - 1 : i
+          )
+          .filter((i) => i < nPages)
+          .map((i) => {
+            return (
+              <li key={`p${i}`}>
+                <button
+                  onClick={() => set(size * i)}
+                  onKeyDown={() => set(size * i)}
+                  type="button"
+                  disabled={i < 0}
+                  className={`pagination-link ${
+                    i === currentPage && `current`
+                  }`}
+                >
+                  <span>{i < 0 ? "1" : i + 1}</span>
+                </button>
+              </li>
+            );
+          })}
+        <li>
+          <button
+            onClick={() => set((nPages - 1) * size)}
+            onKeyDown={() => set((nPages - 1) * size)}
+            type="button"
+            className="pagination-last"
+            disabled={currentPage === nPages - 1}
+          >
+            <span>Last</span> <SVGIcon name="chevronRight" />
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
