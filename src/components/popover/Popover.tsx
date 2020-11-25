be289cc0a9dc6d622e4438ea91ed9c8c9513802c
nazarhoray <nazar.horay@nordcloud.com>
2020-11-25 10:41:22 +0100

    add alignRight prop for popover

--- src/components/popover/Popover.tsx
+++ src/components/popover/Popover.tsx
@@ -2,40 +2,45 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import theme from "../../theme";
 import { useClickOutside } from "../../hooks";
+import { Box } from "../box";
 
 type ContentData = {
   isVisible: boolean;
+  [x: string]: any;
 };
 
 type PopoverProps = {
   children?: React.ReactNode;
   trigger?: React.ReactNode;
+  alignRight?: boolean;
 };
 
 const StyledPopover = styled.div`
   display: inline-block;
+  position: relative;
 `;
 
 const Content = styled.div`
-  display: flex;
-  flex-direction: column;
   visibility: hidden;
   opacity: 0;
   position: absolute;
   z-index: ${theme.zindex.dropdown};
-  box-shadow: ${theme.shadow.shadow02};
-  background-color: ${theme.colors.white};
-  border-radius: ${theme.radius.md};
-  padding: ${theme.spacing.spacing04};
   transition: visibility 0.1s ease-out, opacity 0.1s ease-out;
   margin-top: ${theme.spacing.spacing01};
-
   ${({ isVisible }: ContentData) =>
     isVisible &&
     `
     visibility: visible;
     opacity: 1;
   `}
+  ${({ alignRight }: PopoverProps) =>
+    alignRight &&
+    `
+      right: 0;
+      &>div {
+        text-align: right;
+      }
+    `}
 `;
 
 const StyledTrigger = styled.div`
@@ -43,7 +48,7 @@ const StyledTrigger = styled.div`
   cursor: pointer;
 `;
 
-export const Popover = ({ children, trigger }: PopoverProps) => {
+export const Popover = ({ children, trigger, alignRight }: PopoverProps) => {
   const [isVisible, setIsVisible] = useState(false);
   const toggle = () => {
     setIsVisible(!isVisible);
@@ -59,7 +64,11 @@ export const Popover = ({ children, trigger }: PopoverProps) => {
       <StyledTrigger className="Popover-trigger" onClick={toggle}>
         {trigger}
       </StyledTrigger>
-      <Content isVisible={isVisible}>{children}</Content>
+      <Content isVisible={isVisible} alignRight={alignRight}>
+        <Box innerSpacing="spacing03" shadow="shadow04">
+          {children}
+        </Box>
+      </Content>
     </StyledPopover>
   );
 };
