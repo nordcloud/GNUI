8671069de66ccb5d0ae9a0bdb5bb386b72dd4a1b
Luke <fire_fox_liu@hotmail.com>
2021-06-09 16:35:06 +0800

    feature: new component TabsV2 - customize heading

---
+++ src/components/tabsv2/TabsV2.tsx
@@ -0,0 +1,296 @@
+import React, { FunctionComponent } from "react";
+import styled, { css } from "styled-components";
+import theme from "../../theme";
+import { Text } from "../text";
+import { Box } from "../box";
+import { Button } from "../button";
+
+type TabProps = {
+  className?: string;
+  wizard?: boolean;
+  step?: number;
+  heading?: React.ReactNode;
+  caption?: string;
+  activeTab?: number;
+  children?: React.ReactNode;
+  onClick?: (e: any) => void;
+  onChange?: (e: any) => void;
+  props?: any;
+  styleActive?: boolean;
+  index?: number;
+  disabled?: boolean;
+  buttons?: React.ReactNode;
+  buttonsJustify?: string;
+};
+
+type TabsProps = {
+  wizard?: boolean;
+  name?: string;
+  caption?: string;
+  children: Array<TabProps>;
+  handleTab: (e: any) => void;
+  step: number;
+};
+
+type ButtonPreviousProps = {
+  onClick?: (e: any) => void;
+};
+
+type ButtonNextProps = {
+  onClick?: (e: any) => void;
+};
+
+export const TabsV2Content = styled(Box)`
+  background-color: ${theme.color.background.ui01};
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  box-shadow: none;
+  padding: ${theme.spacing.spacing06} ${theme.spacing.spacing04};
+  p {
+    line-height: 1.5rem;
+    &:first-child {
+      margin-top: 0;
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+export const TabV2Container: any = styled.div<TabProps>`
+  padding: ${theme.spacing.spacing04};
+  background-color: ${theme.color.background.ui03};
+  border-right: 1px solid ${theme.color.border.border01};
+  border-bottom: 1px solid transparent;
+  width: 17rem;
+  &:last-child {
+    border-right: none;
+    &.tab-active {
+      border-right: 1px solid ${theme.color.border.border01};
+    }
+  }
+
+  &:hover {
+    cursor: ${({ disabled }) => (disabled ? `auto` : `pointer`)};
+    background-color: ${({ disabled }) =>
+      disabled
+        ? `${theme.color.background.ui03}`
+        : `${theme.color.background.ui01}`};
+  }
+
+  ${({ wizard }) =>
+    wizard &&
+    css`
+      width: auto;
+      flex: 1;
+      &:last-child {
+        border-right: none;
+        &.tab-active {
+          border-right: none;
+        }
+      }
+    `}
+  &:first-child {
+    border-top-left-radius: ${theme.radiusDefault};
+  }
+  &.tab-active {
+    background-color: ${theme.color.background.ui01};
+    border-bottom: 1px solid ${theme.color.background.ui01};
+    z-index: ${theme.zindex.default};
+  }
+
+  &.tab,
+  &.tab-active {
+    &:first-child {
+      & > div:first-child {
+        background-color: ${({ wizard }) =>
+          wizard ? theme.color.background.ui05 : ""};
+        color: ${theme.color.text.text04};
+      }
+    }
+  }
+`;
+
+const TabsList = styled.div`
+  background-color: ${theme.color.background.ui03};
+  display: flex;
+  overflow-x: auto;
+  padding: 0;
+  margin: 0;
+  position:relative;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  &::after {
+    content:"";
+    width:100%;
+    position:absolute;
+    left:0;
+    bottom:0;
+    border-bottom: 1px solid ${theme.color.border.border01};
+    z-index: ${theme.zindex.zero};
+  }
+}
+`;
+
+const TabsWrapper = styled(Box)`
+  padding: 0;
+`;
+
+const Step = styled(Box)`
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2rem;
+  height: 2rem;
+  box-shadow: none;
+  margin-bottom: ${theme.spacing.spacing04};
+  background-color: ${theme.color.background.ui04};
+  color: ${theme.color.text.text02};
+
+  &.dark {
+    background-color: ${theme.color.background.ui05};
+    color: ${theme.color.text.text04};
+  }
+`;
+
+type StyledTabsStatusButtonsProps = {
+  buttonsJustify?: string;
+};
+
+const TabsStatusButtons = styled.div<StyledTabsStatusButtonsProps>`
+  padding: ${theme.spacing.spacing04};
+  border-top: 1px solid ${theme.color.border.border01};
+  display: flex;
+  justify-content: ${({ buttonsJustify }) =>
+    buttonsJustify ? buttonsJustify : "space-between"};
+  position: relative;
+`;
+
+const TabsCover = styled.div`
+  background-color: ${theme.color.background.ui01};
+  border-bottom-left-radius: ${theme.radiusDefault};
+  border-bottom-right-radius: ${theme.radiusDefault};
+`;
+
+const PreviousButton = styled(Button)`
+  position: absolute;
+  border: none;
+`;
+const NextButton = styled(Button)`
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const ButtonV2Previous: FunctionComponent<ButtonPreviousProps> = ({
+  onClick,
+  children,
+}) => {
+  return <PreviousButton onClick={onClick}>{children}</PreviousButton>;
+};
+
+export const ButtonV2Next: FunctionComponent<ButtonNextProps> = ({
+  onClick,
+  children,
+}) => {
+  return <NextButton onClick={onClick}>{children}</NextButton>;
+};
+
+export const TabV2: FunctionComponent<TabProps> = ({
+  wizard,
+  step,
+  heading,
+  caption,
+  activeTab,
+  index,
+  disabled,
+  onClick,
+  buttons,
+  buttonsJustify,
+}) => {
+  const className = activeTab === index ? "tab-active" : "tab";
+  /* eslint-disable */
+  return (
+    <TabV2Container
+      className={className}
+      onClick={onClick}
+      key={index}
+      disabled={disabled}
+      wizard={wizard}
+      buttons={buttons}
+      buttonsJustify={buttonsJustify}
+    >
+      {wizard ? (
+        activeTab && index ? (
+          <Step {...(index <= activeTab && { className: "dark" })}>{step}</Step>
+        ) : (
+          <Step>{step}</Step>
+        )
+      ) : null}
+
+      <React.Fragment>{heading}</React.Fragment>
+
+      <Text size="sm" mb={0} color={theme.color.text.text02}>
+        {caption}
+      </Text>
+    </TabV2Container>
+  );
+};
+
+export const TabsV2: FunctionComponent<TabsProps> = ({
+                                                       name,
+                                                       wizard,
+                                                       children,
+                                                       handleTab,
+                                                       step,
+                                                     }) => {
+  const items = React.Children.toArray(children) as TabProps[];
+  /* eslint-disable */
+  return (
+    <TabsWrapper>
+      <TabsList className={name}>
+        {items.map((child, key) => {
+          const { heading, caption, disabled, buttonsJustify } = child.props;
+          return (
+            <TabV2
+              key={key}
+              wizard={wizard}
+              step={key + 1}
+              activeTab={step}
+              index={key}
+              heading={heading}
+              caption={caption}
+              disabled={disabled}
+              buttonsJustify={buttonsJustify}
+              onClick={disabled ? undefined : () => handleTab(key)}
+            />
+          );
+        })}
+      </TabsList>
+      <TabsCover>
+        {items.map((child, key) => {
+          if (key !== step) return undefined;
+          const { children, buttonsJustify, buttons } = child.props;
+          return (
+            <React.Fragment key={key}>
+              <TabsV2Content>{children}</TabsV2Content>
+              {buttons != null && (
+                <TabsStatusButtons buttonsJustify={buttonsJustify}>
+                  {buttons}
+                </TabsStatusButtons>
+              )}
+            </React.Fragment>
+          );
+        })}
+      </TabsCover>
+    </TabsWrapper>
+  );
+};
+
+export const StyledTabV2: FunctionComponent<ButtonNextProps> = ({
+                                                                  onClick,
+                                                                  children,
+                                                                }) => {
+  return <NextButton onClick={onClick}>{children}</NextButton>;
+};
