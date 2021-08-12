373cdf76fc91802aaa642eb0ca0a4697b1967a37
Przemysław Gosek <przemyslaw.gosek@nordcloud.com>
2021-08-12 11:21:46 +0200

    Change font-family of Select buttons to Rubik

--- src/components/selectbutton/SelectButton.tsx
+++ src/components/selectbutton/SelectButton.tsx
@@ -77,6 +77,7 @@ const StyledSelectButtons = styled.ul<StyledSelectButtons>`
       padding: ${theme.spacing.spacing02} ${theme.spacing.spacing04};
       transition: ${theme.transition};
       font-size: ${theme.fontSizes.md};
+      font-family: ${theme.fonts.body};
       line-height: 1.5;
       text-transform: ${theme.typography.titleCase};
 
