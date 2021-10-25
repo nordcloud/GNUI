4d427b0ac56218142e4f9b20ea607563a68df36e
nowyDEV <dominik.nowik@nordcloud.com>
2021-10-25 15:46:03 +0200

    fix types for RadioGroup component

--- src/components/radiobutton/Radiobutton.tsx
+++ src/components/radiobutton/Radiobutton.tsx
@@ -110,16 +110,19 @@ export const Radio = React.forwardRef<HTMLInputElement, RadioProps>(
   )
 );
 
+type RadioGroupChild = React.ReactElement<React.ComponentProps<typeof Radio>>;
+
 export type RadioGroupProps = {
   name: string;
-  children: React.ReactElement<React.ComponentProps<typeof Radio>>;
+  children: RadioGroupChild | RadioGroupChild[];
 };
 
 export function RadioGroup({ name, children }: RadioGroupProps) {
-  const hasNoChildren = Array.isArray(children) && children.length === 0;
   const [isChecked, setIsChecked] = React.useState(
-    Array.isArray(children) && children[0]?.props?.value
+    Array.isArray(children) && children[0].props.value
   );
+
+  const hasNoChildren = Array.isArray(children) && children.length === 0;
 
   if (hasNoChildren) {
     return null;
