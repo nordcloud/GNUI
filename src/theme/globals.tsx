afd2e35f5e7cfd908cb5e4e6156ecd87d9fd177c
Andrzej Gonerski <andrev321@gmail.com>
2021-11-08 13:54:37 +0100

    CR improvements

--- src/theme/globals.tsx
+++ src/theme/globals.tsx
@@ -39,49 +39,14 @@ const GlobalStyle = createGlobalStyle<Props>`
     }
   }
 
-  ${({ customScrollbars }) =>
-    customScrollbars &&
-    css`
-      html {
-        scrollbar-face-color: ${theme.color.support.grey};
-        scrollbar-base-color: ${theme.color.support.grey};
-        scrollbar-highlight-color: ${theme.color.support.grey};
-        scrollbar-track-color: ${theme.color.background.ui01};
-        scrollbar-arrow-color: ${theme.color.background.ui01};
-        scrollbar-shadow-color: ${theme.color.support.grey};
-        scrollbar-dark-shadow-color: ${theme.color.support.grey};
-      }
-
-      ::-webkit-scrollbar {
-        width: 14px;
-        height: 14px;
-      }
-      ::-webkit-scrollbar-track-piece {
-        background-color: ${theme.color.background.ui01};
-      }
-      ::-webkit-scrollbar-thumb {
-        height: 10px;
-        background-color: ${theme.color.support.grey};
-        border-radius: 5px;
-      }
-      ::-webkit-scrollbar-corner {
-        background-color: ${theme.color.background.ui01};
-      }
-
-      body * {
-        ::-webkit-scrollbar {
-          width: 10px;
-          height: 10px;
-        }
-      }
-    `}
+  ${({ customScrollbars }) => customScrollbars && scrollbarCustomStyles}
 `;
 
 /**
  * Component for setting global styles using GNUI theme.
- * @param {boolean} [customScrollbars] - Option to enable GNUI theme based styling for scrollbars.
+ * @param {boolean} [customScrollbars=false] - Option to enable GNUI theme based styling for scrollbars.
  */
-export function SetGlobalStyle({ customScrollbars }: Props) {
+export function SetGlobalStyle({ customScrollbars = false }: Props) {
   useThemeSwitcher();
   return (
     <>
@@ -101,3 +66,38 @@ export function SetGlobalStyle({ customScrollbars }: Props) {
     </>
   );
 }
+
+const scrollbarCustomStyles = css`
+  html {
+    scrollbar-face-color: ${theme.color.support.grey};
+    scrollbar-base-color: ${theme.color.support.grey};
+    scrollbar-highlight-color: ${theme.color.support.grey};
+    scrollbar-track-color: ${theme.color.background.ui01};
+    scrollbar-arrow-color: ${theme.color.background.ui01};
+    scrollbar-shadow-color: ${theme.color.support.grey};
+    scrollbar-dark-shadow-color: ${theme.color.support.grey};
+  }
+
+  ::-webkit-scrollbar {
+    width: 14px;
+    height: 14px;
+  }
+  ::-webkit-scrollbar-track-piece {
+    background-color: ${theme.color.background.ui01};
+  }
+  ::-webkit-scrollbar-thumb {
+    height: 10px;
+    background-color: ${theme.color.support.grey};
+    border-radius: 5px;
+  }
+  ::-webkit-scrollbar-corner {
+    background-color: ${theme.color.background.ui01};
+  }
+
+  body * {
+    ::-webkit-scrollbar {
+      width: 10px;
+      height: 10px;
+    }
+  }
+`;
