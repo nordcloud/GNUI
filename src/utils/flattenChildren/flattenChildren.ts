19c115bc0c5852b68b7724b787526757ff3efef2
Andrzej Gonerski <andrev321@gmail.com>
2021-11-15 14:46:32 +0100

    Simplify flattenChildren implementation

--- src/utils/flattenChildren/flattenChildren.ts
+++ src/utils/flattenChildren/flattenChildren.ts
@@ -6,15 +6,11 @@ type ReactChildArray = ReturnType<typeof React.Children.toArray>;
 
 export function flattenChildren(children: React.ReactNode): ReactChildArray {
   const childrenArray = React.Children.toArray(children);
-
-  return childrenArray.reduce((flatChildren: ReactChildArray, child) => {
+  return childrenArray.flatMap((child) => {
     if ((child as React.ReactElement<any>).type === React.Fragment) {
-      return flatChildren.concat(
-        flattenChildren((child as React.ReactElement<any>).props.children)
-      );
+      return flattenChildren((child as React.ReactElement<any>).props.children);
     }
 
-    flatChildren.push(child);
-    return flatChildren;
-  }, []);
+    return child;
+  });
 }
