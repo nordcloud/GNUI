23790055c71ee98318b5d4106e93ddf7ec1e13b7
Andrzej Gonerski <andrev321@gmail.com>
2021-11-15 11:45:06 +0100

    CR fix

--- src/utils/flattenChildren/flattenChildren.ts
+++ src/utils/flattenChildren/flattenChildren.ts
@@ -6,12 +6,14 @@ type ReactChildArray = ReturnType<typeof React.Children.toArray>;
 
 export function flattenChildren(children: React.ReactNode): ReactChildArray {
   const childrenArray = React.Children.toArray(children);
+
   return childrenArray.reduce((flatChildren: ReactChildArray, child) => {
     if ((child as React.ReactElement<any>).type === React.Fragment) {
       return flatChildren.concat(
         flattenChildren((child as React.ReactElement<any>).props.children)
       );
     }
+    
     flatChildren.push(child);
     return flatChildren;
   }, []);
