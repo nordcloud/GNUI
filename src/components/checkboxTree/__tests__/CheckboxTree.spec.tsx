4b8e3c3276192579f38fb37b82934fcc628b393d
ignasveleckisnc <88425985+ignasveleckisnc@users.noreply.github.com>
2021-12-22 14:16:38 +0200

    KLAR-3040 Add CheckboxTree component (#394)

---
+++ src/components/checkboxTree/__tests__/CheckboxTree.spec.tsx
@@ -0,0 +1,89 @@
+/* Copyright (c) 2020-2021 Nordcloud Oy or its affiliates. All Rights Reserved. */
+
+import * as React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import { CheckboxTree } from "../CheckboxTree";
+import { CheckboxTreeProps } from "../types";
+
+const data = [
+  {
+    uid: "meat",
+    label: "Meat",
+    children: [
+      {
+        uid: "pork",
+        label: "Pork",
+        children: [
+          {
+            uid: "ham",
+            label: "Ham",
+          },
+          {
+            uid: "bacon",
+            label: "Bacon",
+            children: [
+              {
+                uid: "15%",
+                label: "15%",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        uid: "chicken",
+        label: "Chicken",
+        children: [
+          {
+            uid: "cbreast",
+            label: "Breast",
+          },
+        ],
+      },
+      {
+        uid: "turkey",
+        label: "Turkey",
+        children: [
+          {
+            uid: "tbreast",
+            label: "Breast",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const renderComponent = ({
+  composition,
+  selected,
+  expanded,
+}: CheckboxTreeProps) =>
+  render(
+    <CheckboxTree
+      composition={composition}
+      selected={selected}
+      expanded={expanded}
+    />
+  );
+
+test("setting pre-expanded items", async () => {
+  renderComponent({
+    composition: data,
+    expanded: ["meat", "meat->pork"],
+  });
+
+  await screen.findByText("Pork");
+  await screen.findByText("Ham");
+});
+
+test("setting pre-selected items", async () => {
+  const component = renderComponent({
+    composition: data,
+    selected: ["meat"],
+  });
+  const checkbox = component.container.querySelector("#meat");
+
+  expect(checkbox).toHaveProperty("checked", true);
+});
---
+++ src/components/checkboxTree/utils.ts
@@ -0,0 +1,116 @@
+import { Composition } from "./types";
+
+export const preProcessTree = (
+  tree: Composition[],
+  separator = "->"
+): Composition[] => {
+  const treeClone = JSON.parse(JSON.stringify(tree));
+  const mappedUids = getRelations(tree, separator);
+
+  return attachRelations(treeClone, mappedUids, separator);
+};
+
+export const getChildrenUids = (composition: Composition): string[] => {
+  const uids: string[] = [];
+
+  if (!composition.children) {
+    return [];
+  }
+
+  const recursivelyIterateChildren = (children: Composition[]) => {
+    children.forEach((child) => {
+      uids.push(child.uid);
+      if (child.children) {
+        recursivelyIterateChildren(child.children);
+      }
+    });
+  };
+  recursivelyIterateChildren(composition.children);
+
+  return uids;
+};
+
+export const getParentsUids = (
+  compositionUid: string,
+  separator = "->"
+): string[] => {
+  const currentIdSpilt = compositionUid.split(separator);
+
+  return currentIdSpilt.map((_, index) =>
+    [...currentIdSpilt].splice(0, index + 1).join(separator)
+  );
+};
+
+export const getRelations = (tree: Composition[], separator = "->") => {
+  const allRelations = [];
+
+  let branch: string[] = [];
+  let treeCopy = [...tree];
+  let isTreeEmpty = false;
+
+  const firstChildOfItem = (childs: Composition[]) => {
+    const firstChild = childs ? childs[0] : null;
+    if (!firstChild) {
+      return;
+    }
+    branch.push(firstChild.uid);
+    if (firstChild.children && firstChild.children.length > 0) {
+      firstChildOfItem(firstChild.children);
+    } else {
+      treeCopy = removeObject(treeCopy, firstChild.uid);
+    }
+  };
+
+  while (!isTreeEmpty) {
+    firstChildOfItem(treeCopy);
+    allRelations.push([...new Set(branch)].join(separator));
+    branch = [];
+    isTreeEmpty = treeCopy.length === 0;
+  }
+
+  return allRelations;
+};
+
+const removeObject = (tree: Composition[], uid: string) => {
+  const loop = (childsArray: Composition[] | undefined) => {
+    if (childsArray) {
+      for (let index in childsArray) {
+        if (childsArray[index].uid === uid) {
+          childsArray.splice(Number(index), 1);
+        } else {
+          if (childsArray[index].children) {
+            loop(childsArray[index].children);
+          }
+        }
+      }
+    }
+  };
+  loop(tree);
+
+  return tree;
+};
+
+const attachRelations = (
+  tree: Composition[],
+  relations: string[],
+  separator = "->"
+) => {
+  const recursivelyIterateChildren = (children: Composition[]) => {
+    children.forEach((child) => {
+      relations.forEach((relation) => {
+        const relationUids = relation.split(separator);
+        const lastUid = relationUids[relationUids.length - 1];
+
+        if (lastUid === child.uid) {
+          child.uid = relation;
+        }
+      });
+      if (child.children) {
+        recursivelyIterateChildren(child.children);
+      }
+    });
+  };
+  recursivelyIterateChildren(tree);
+
+  return tree;
+};
