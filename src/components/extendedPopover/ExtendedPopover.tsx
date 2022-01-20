c47fd35bdd1c08e2605067f409ab29b763a2b895
Marcin Strażyński <marcin.strazynski@nordcloud.com>
2022-01-20 09:54:48 +0100

    New tooltip and popover (#421)

---
+++ src/components/extendedPopover/ExtendedPopover.tsx
@@ -0,0 +1,167 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import styled from "styled-components";
+import { useEvent } from "../../hooks";
+import theme from "../../theme";
+import {
+  Placement,
+  Position,
+  getStyle,
+  Margin,
+  DEFAULT_MARGIN,
+  PaddingWrapper,
+} from "../../utils/position";
+import { throttle } from "../../utils/throttle";
+import { Button } from "../button";
+import { SVGIcon } from "../svgicon";
+
+type Props = {
+  content: React.ReactNode;
+  trigger?: React.ReactNode;
+  placement?: Placement;
+  position?: Position;
+  margin?: Margin;
+  clickOutsideToClose?: boolean;
+  triggerOn?: ExtendedPopoverAction;
+  closeOn?: ExtendedPopoverAction;
+};
+
+export function ExtendedPopover({
+  trigger = (
+    <Button size="sm">
+      <SVGIcon size="sm" name="burgerMenu" />
+    </Button>
+  ),
+  content,
+  placement = "right",
+  position = "start",
+  clickOutsideToClose = true,
+  margin = DEFAULT_MARGIN,
+  triggerOn = "click",
+  closeOn = "click",
+}: Props) {
+  const triggerRef = React.useRef<HTMLDivElement>(null);
+  const contentRef = React.useRef<HTMLDivElement>(null);
+
+  const [open, setOpen] = React.useState(false);
+  const [contentDimensions, setContentDimensions] =
+    React.useState<DOMRect | null>(null);
+  const [triggerDimensions, setTriggerDimensions] =
+    React.useState<DOMRect | null>(null);
+
+  React.useEffect(() => {
+    if (
+      open &&
+      contentRef.current instanceof HTMLDivElement &&
+      triggerRef.current instanceof HTMLDivElement
+    ) {
+      setTriggerDimensions(triggerRef.current.getBoundingClientRect());
+      setContentDimensions(contentRef.current.getBoundingClientRect());
+    }
+  }, [open]);
+
+  const handleClickOutside = React.useCallback(
+    (event) => {
+      if (
+        clickOutsideToClose &&
+        contentRef.current &&
+        triggerRef.current &&
+        !contentRef.current.contains(event.target) &&
+        !triggerRef.current.contains(event.target)
+      ) {
+        setOpen(false);
+      }
+    },
+    [contentRef, triggerRef, clickOutsideToClose]
+  );
+
+  useEvent({ name: "click", handler: handleClickOutside, target: document });
+
+  const handleScroll = throttle(
+    React.useCallback(() => {
+      setOpen(false);
+    }, [])
+  );
+
+  useEvent({ name: "scroll", handler: handleScroll });
+
+  if (content == null) {
+    return null;
+  }
+
+  const triggerProps = {
+    ...(triggerOn === "click"
+      ? {
+          onClick: () => setOpen((prevState) => !prevState),
+        }
+      : {
+          onMouseEnter: () => setOpen(true),
+          onMouseLeave: () => {
+            if (closeOn === "hover") {
+              setOpen(false);
+            }
+          },
+        }),
+  };
+
+  const style = getStyle({
+    wrapperDimensions: triggerDimensions,
+    tooltipDimensions: contentDimensions,
+    placement,
+    position,
+  });
+
+  return (
+    <TriggerWrapper ref={triggerRef} {...triggerProps}>
+      {trigger}
+      <Popover
+        content={content}
+        contentRef={contentRef}
+        visible={open}
+        style={style}
+        margin={margin}
+        placement={placement}
+      />
+    </TriggerWrapper>
+  );
+}
+
+type PopoverProps = {
+  content: React.ReactNode;
+  contentRef: React.RefObject<HTMLDivElement>;
+  visible: boolean;
+  style: { top?: number; left?: number };
+  margin: Margin;
+  placement: Placement;
+};
+
+function Popover({
+  contentRef,
+  visible,
+  style,
+  margin,
+  content,
+  placement,
+}: PopoverProps) {
+  return visible
+    ? ReactDOM.createPortal(
+        <ContentWrapper ref={contentRef} style={style}>
+          <PaddingWrapper {...margin} placement={placement}>
+            {content}
+          </PaddingWrapper>
+        </ContentWrapper>,
+        document.body
+      )
+    : null;
+}
+
+const TriggerWrapper = styled.div`
+  display: inline-block;
+`;
+
+const ContentWrapper = styled.div`
+  position: fixed;
+  z-index: ${theme.zindex.sticky};
+`;
+
+export type ExtendedPopoverAction = "hover" | "click";
---
+++ src/components/extendedTooltip/ExtendedTooltip.tsx
@@ -0,0 +1,161 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import styled, { css } from "styled-components";
+import { useEvent } from "../../hooks";
+import theme from "../../theme";
+import {
+  DEFAULT_MARGIN,
+  getStyle,
+  Margin,
+  Placement,
+  Position,
+} from "../../utils/position";
+import { setColor } from "../../utils/setcolor";
+import { useTooltipHover } from "../tooltip/hooks";
+
+type Timeout = {
+  showTimeout?: number;
+  hideTimeout?: number;
+};
+
+type Status = "danger" | "warning" | "success" | "notification";
+
+type Props = Timeout & {
+  caption: React.ReactNode;
+  children: React.ReactNode;
+  placement?: Placement;
+  position?: Position;
+  margin?: Margin;
+  status?: Status;
+};
+
+export function ExtendedTooltip({
+  caption,
+  children,
+  placement = "top",
+  position = "center",
+  status,
+  showTimeout = 0,
+  hideTimeout = 0,
+  margin = DEFAULT_MARGIN,
+}: Props) {
+  const wrapperRef = React.useRef<HTMLDivElement>(null);
+  const tooltipRef = React.useRef<HTMLDivElement>(null);
+
+  const { isHovered, updateIsHovered } = useTooltipHover();
+
+  const [tooltipDimensions, setTooltipDimensions] =
+    React.useState<DOMRect | null>(null);
+  const [wrapperDimensions, setWrapperDimensions] =
+    React.useState<DOMRect | null>(null);
+
+  const handleScroll = React.useCallback(() => {
+    updateIsHovered(false, 0);
+  }, [updateIsHovered]);
+
+  useEvent({ name: "scroll", handler: handleScroll });
+
+  React.useEffect(() => {
+    if (
+      isHovered &&
+      tooltipRef.current instanceof HTMLDivElement &&
+      wrapperRef.current instanceof HTMLDivElement
+    ) {
+      setTooltipDimensions(tooltipRef.current.getBoundingClientRect());
+      setWrapperDimensions(wrapperRef.current.getBoundingClientRect());
+    }
+  }, [isHovered]);
+
+  if (caption == null) {
+    return <>{children}</>;
+  }
+
+  const style = getStyle({
+    wrapperDimensions,
+    tooltipDimensions,
+    placement,
+    position,
+    margin,
+  });
+
+  return (
+    <TooltipWrapper
+      ref={wrapperRef}
+      onMouseEnter={() => updateIsHovered(true, showTimeout)}
+      onMouseLeave={() => updateIsHovered(false, hideTimeout)}
+    >
+      <Tooltip
+        isHovered={isHovered}
+        caption={caption}
+        tooltipRef={tooltipRef}
+        status={status}
+        style={style}
+      />
+      {children}
+    </TooltipWrapper>
+  );
+}
+
+type TooltipProps = {
+  caption: React.ReactNode;
+  isHovered: boolean;
+  tooltipRef: React.RefObject<HTMLDivElement>;
+  style: { top?: number; left?: number };
+  status?: Status;
+};
+
+function Tooltip({
+  caption,
+  isHovered,
+  tooltipRef,
+  status,
+  style,
+}: TooltipProps) {
+  return isHovered
+    ? ReactDOM.createPortal(
+        <StyledTooltip ref={tooltipRef} status={status} style={style}>
+          {caption}
+        </StyledTooltip>,
+        document.body
+      )
+    : null;
+}
+
+type StyledTooltipProps = Pick<Props, "status">;
+
+const StyledTooltip = styled.div<StyledTooltipProps>`
+  position: fixed;
+  min-width: 0;
+  max-width: 16rem;
+  font-size: ${theme.fontSizes.sm};
+  line-height: 1rem;
+  padding: ${theme.spacing.spacing02};
+  background-color: ${theme.color.background.ui05};
+  color: ${theme.color.text.text04};
+  border-radius: ${theme.radius.md};
+  z-index: ${theme.zindex.sticky};
+  opacity: 0;
+  animation: 0.2s ease-in-out both fadeIn;
+
+  ${({ status }) => status && getColor(status)}
+
+  @keyframes fadeIn {
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const TooltipWrapper = styled.div`
+  display: inline-block;
+`;
+
+function getColor(status: Status) {
+  return css`
+    background-color: ${setColor(status)};
+    color: ${theme.color.text.text01};
+    &:after {
+      border-top-color: ${setColor(status)};
+    }
+  `;
+}
