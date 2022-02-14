aea1d1edf396786f5f8ca8b0f7e9ed739a158b82
Marcin Strażyński <marcin.strazynski@nordcloud.com>
2022-02-14 09:53:18 +0100

    fixed test

--- src/components/navigationBar/NavigationBar.spec.tsx
+++ src/components/navigationBar/NavigationBar.spec.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { NavigationBar } from "./NavigationBar";
 import { MenuItem } from "./MenuItem";
+import { NavigationBar } from "./NavigationBar";
 
 test("renders hamburger for expanding", () => {
   const { container } = renderDummyNavBar();
@@ -14,42 +14,53 @@ test("renders hamburger for expanding", () => {
 test("doesn't render hamburger when expanding is disabled", () => {
   const { container } = renderDummyNavBar(false);
 
-  expect(container.querySelector("svg")).not.toBeInTheDocument();  
+  expect(container.querySelector("svg")).not.toBeInTheDocument();
 });
 
-test("renders MenuItems as icons without text", () => {
+test("renders MenuItems as icons without text when folded", () => {
   const { container } = renderNavBarWithItems();
-  
+
   expect(container.querySelectorAll("svg")).toHaveLength(3);
-  expect(screen.queryByText("Dashboard")).toBe(null);
+  expect(screen.queryByText("Dashboard")).toBeNull();
 });
 
-// To be refined, onAnimationEnd is not triggered
-test("renders MenutItems as icons with text (when unfolded)", async () => {
+test("renders MenutItems as icons with text when unfolded", async () => {
   const { container } = renderNavBarWithItems();
 
-  expect(screen.queryByText("Dashboard")).toBe(null);
+  expect(screen.queryByText("Dashboard")).toBeNull();
 
   const hamburger = container.querySelector("svg");
-  
+
   if (hamburger) {
     userEvent.click(hamburger);
-    fireEvent.transitionEnd(container);
   }
 
-  const text = await waitFor(() => screen.queryByText("Dashboard"));
-  expect(text).not.toBeInTheDocument();
+  if (container.firstChild) {
+    fireEvent.transitionEnd(container.firstChild);
+  }
+
+  const text = await screen.findByText("Dashboard");
+  expect(text).toBeInTheDocument();
 });
 
-function renderDummyNavBar(showHamburger: boolean = true) {
-  return render(<NavigationBar expandable={showHamburger}>
-    <a href="https://www.nordcloud.com" target="_blank">Link</a>
-  </NavigationBar>);
+function renderDummyNavBar(showHamburger = true) {
+  return render(
+    <NavigationBar
+      expandable={showHamburger}
+      expandableConfig={{ duration: "0" }}
+    >
+      <a href="https://www.nordcloud.com" target="_blank" rel="noreferrer">
+        Link
+      </a>
+    </NavigationBar>
+  );
 }
 
-function renderNavBarWithItems(showHamburger: boolean = true) {
-  return render(<NavigationBar expandable={showHamburger}>
-    <MenuItem caption="Dashboard" icon="dashboard" />
-    <MenuItem caption="Cloud" icon="cloud" />
-  </NavigationBar>)
+function renderNavBarWithItems(showHamburger = true) {
+  return render(
+    <NavigationBar expandable={showHamburger}>
+      <MenuItem caption="Dashboard" icon="dashboard" />
+      <MenuItem caption="Cloud" icon="cloud" />
+    </NavigationBar>
+  );
 }
