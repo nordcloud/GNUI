207a1ae90cdd477a08045312f72c0c73f6c1e0c2
nowyDEV <dominik.nowik@nordcloud.com>
2022-09-19 16:23:43 +0200

    optimize builds

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,15 @@
 module.exports = {
   stories: ["../src/**/*.stories.@(js|mdx)"],
-  addons: ["@storybook/addon-docs"],
+  addons: [
+    "@storybook/addon-docs",
+    {
+      // https://storybook.js.org/addons/storybook-addon-turbo-build
+      name: "storybook-addon-turbo-build",
+      options: {
+        optimizationLevel: 3,
+      },
+    },
+  ],
   preLoaders: [
     {
       test: /\.tsx$/,
