148f7c3ad35be14231fb5bed5e017fe864e9772e
nazarhoray <nazar.horaj@gmail.com>
2022-11-09 16:48:59 +0100

    font-family update

--- src/theme/globals.tsx
+++ src/theme/globals.tsx
@@ -59,7 +59,7 @@ export function SetGlobalStyle({ customScrollbars = false }: Props) {
           crossOrigin=""
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
       </Helmet>
