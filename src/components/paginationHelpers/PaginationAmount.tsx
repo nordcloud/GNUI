d789557e4d9f96c5a06726f1c9b731600c822adf
Jan Osio <58164749+janosio-nordcloud@users.noreply.github.com>
2022-12-05 16:26:01 +0100

    [Feature] Pagination - add compatibility with API indexes pages from 1 (#534)
    * Add index of first page
    
    * Add index of first page
    
    * Add index of first page
    
    * fix
    
    * Add tests
    
    * fix

--- src/components/paginationHelpers/PaginationAmount.tsx
+++ src/components/paginationHelpers/PaginationAmount.tsx
@@ -4,16 +4,25 @@ type Props = {
   from: number;
   size: number;
   count: number;
+  firstPage?: number;
 };
 
-export function PaginationAmount({ from, size, count }: Props) {
+export function PaginationAmount({ from, size, count, firstPage = 0 }: Props) {
+  const isFirstPageZero = firstPage === 0;
+
   const s = Number(size);
-  const maxCurrentPage = count < s ? count : Math.min(from + s);
-  const minCurrentPage = count < s ? "1" : from + 1;
+  const c = firstPage * s;
+  const offset = isFirstPageZero ? 0 : s;
+
+  const minCurrentPage = count < s ? 1 : from + 1 - offset;
+  const maxCurrentPage = count < s ? count : Math.min(from + 1 + s) - c - 1;
+
+  const min = minCurrentPage < 1 ? 1 : minCurrentPage;
+  const max = maxCurrentPage < 1 ? size : maxCurrentPage;
+
   return (
     <div className="pagination-result">
-      {minCurrentPage} -{maxCurrentPage > count ? count : maxCurrentPage} of{" "}
-      {count}
+      {min} -{max > count ? count : max} of {count}
     </div>
   );
 }
