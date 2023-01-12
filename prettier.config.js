c94e72f298dd16cbaf64391177f5ffd39a91aec8
nowyDEV <dominik.nowik@nordcloud.com>
2023-01-12 10:36:32 +0100

    bump eslint config, finish renovate config

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   ...require("@nordcloud/eslint-config-pat/prettier.config.js"),
+  overrides: [
+    {
+      files: "*.json5",
+      options: {
+        singleQuote: false,
+        parser: "json5",
+        quoteProps: "preserve",
+        trailingComma: false,
+      },
+    },
+  ],
 };
