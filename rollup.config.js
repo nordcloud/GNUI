3a5e89f5ffc7b8505fa26a088c521bd8e763e770
nowyDEV <dominik.nowik@nordcloud.com>
2023-01-12 13:53:31 +0100

    update rollup to v3

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import typescript from "rollup-plugin-typescript2";
 
 const packageJson = require("./package.json");
 
+/** @type {import("rollup").InputOptions} */
 export default {
   input: "src/index.ts",
   output: [
