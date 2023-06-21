5e9c3094a165c9b090e0f9609d0ba6dd68458727
Dominik Nowik <12304307+nowyDEV@users.noreply.github.com>
2023-06-21 10:23:39 +0200

    Update git hooks setup, add knip (#608)

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import styled from "styled-components";
 import theme from "../../theme";
 
-export type StyledTableProps = {
+type StyledTableProps = {
   hoverline?: boolean;
   striped?: boolean;
   small?: boolean;
