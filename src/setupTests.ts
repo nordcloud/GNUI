da435f61a5c5449c6fbf8811d12cfd01bb3545a6
nowyDEV <dominik.nowik@nordcloud.com>
2023-08-31 13:09:14 +0200

    fix tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,6 @@
-import "@testing-library/jest-dom/extend-expect";
 import { configure } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import "jest-styled-components";
 
 configure({
   asyncUtilTimeout: 3000,
