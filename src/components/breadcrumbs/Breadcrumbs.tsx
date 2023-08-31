a4fb9ae980916e7e068d414db995657fd349c5be
ignasveleckisnc <ignas.veleckis@nordcloud.com>
2023-08-31 17:34:20 +0300

    fix

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import theme from "../../theme";
 
 export type BreadcrumbsList = {
@@ -10,8 +10,48 @@ export type BreadcrumbsList = {
 
 export type BreadcrumbsListProps = {
   list: BreadcrumbsList[];
-  Component?: React.FC<{ to: string }>;
+  Component?: React.FC<{ to: string; isDisabled?: boolean }>;
 };
+
+export function Breadcrumbs({ list, Component }: BreadcrumbsListProps) {
+  return (
+    <StyledBreadcrumbs>
+      <ul>
+        {list.map((br) => (
+          <li key={br.label}>
+            {Component ? (
+              <Component isDisabled={br.isDisabled} css={aStyles} to={br.uri}>
+                {br.label}
+              </Component>
+            ) : (
+              <StyledLink isDisabled={br.isDisabled} href={br.uri}>
+                {br.label}
+              </StyledLink>
+            )}
+          </li>
+        ))}
+      </ul>
+    </StyledBreadcrumbs>
+  );
+}
+
+const aStyles = css`
+  line-height: 1.125rem;
+  font-size: ${theme.fontSizes.sm};
+  font-weight: ${theme.fontWeights.regular};
+  font-family: ${theme.fonts.body};
+  text-decoration: none;
+  transition: ${theme.transition};
+  color: ${theme.color.interactive.link};
+  &:hover,
+  &:focus,
+  &:active {
+    opacity: ${theme.opacity};
+  }
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const StyledBreadcrumbs = styled.nav`
   ul {
@@ -23,6 +63,10 @@ const StyledBreadcrumbs = styled.nav`
       margin-right: ${theme.spacing.spacing02};
       line-height: 1.125rem;
       text-transform: ${theme.typography.titleCase};
+
+      a {
+        ${aStyles}
+      }
 
       &:after {
         content: "/";
@@ -45,47 +89,14 @@ const StyledBreadcrumbs = styled.nav`
   }
 `;
 
-const StyledLink = styled.a<{ isDisabled?: boolean }>`
-  line-height: 1.125rem;
-  font-size: ${theme.fontSizes.sm};
-  font-weight: ${theme.fontWeights.regular};
-  font-family: ${theme.fonts.body};
-  text-decoration: none;
-  transition: ${theme.transition};
-  color: ${(props) =>
-    props.isDisabled ? theme.color.text.text02 : theme.color.interactive.link};
-  pointer-events: ${(props) => (props.isDisabled ? "none" : "auto")};
-  &:hover,
-  &:focus,
-  &:active {
-    opacity: ${(props) => (props.isDisabled ? 1 : theme.opacity)};
-  }
-  &:hover {
-    cursor: ${(props) => (props.isDisabled ? "default" : "pointer")};
-    color: ${(props) =>
-      props.isDisabled
-        ? theme.color.text.text02
-        : theme.color.interactive.link};
-    text-decoration: ${(props) => (props.isDisabled ? "none" : "underline")};
-  }
+const disaledLinkStyles = css`
+  pointer-events: none;
+  color: ${theme.color.text.text02};
 `;
 
-export function Breadcrumbs({ list, Component }: BreadcrumbsListProps) {
-  return (
-    <StyledBreadcrumbs>
-      <ul>
-        {list.map((br) => (
-          <li key={br.label}>
-            {Component ? (
-              <Component to={br.uri || ""}>{br.label}</Component>
-            ) : (
-              <StyledLink isDisabled={br.isDisabled} href={br.uri || ""}>
-                {br.label}
-              </StyledLink>
-            )}
-          </li>
-        ))}
-      </ul>
-    </StyledBreadcrumbs>
-  );
-}
+const StyledLink = styled.a<{ isDisabled?: boolean }>`
+  && {
+    ${aStyles}
+    ${(props) => (props.isDisabled ? disaledLinkStyles : {})}
+  }
+`;
