767cb466e80aa98c34ab77b2cff39d50f640595e
nowyDEV <dominik.nowik@nordcloud.com>
2023-09-12 13:02:11 +0200

    cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.@(js|mdx)"],
-  addons: [
-    "@storybook/addon-docs",
-    // {
-    //   // https://storybook.js.org/addons/storybook-addon-turbo-build
-    //   name: "storybook-addon-turbo-build",
-    //   options: {
-    //     optimizationLevel: 3,
-    //   },
-    // },
-  ],
+  addons: ["@storybook/addon-docs"],
   preLoaders: [
     {
       test: /\.tsx$/,
