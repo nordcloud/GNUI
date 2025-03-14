a9f51150953d808793f58df6ee065533064956c5
Giedrius-Macevicius <giedrius.macevicius@ibm.com>
2025-03-14 13:45:32 +0200

    test(extendedPagination.spec.tsx): add assertions to verify elements are in the document to improve test reliability

--- src/components/extendedPagination/ExtendedPagination.spec.tsx
+++ src/components/extendedPagination/ExtendedPagination.spec.tsx
@@ -38,23 +38,31 @@ const getParams = (firstPage = 0, from = 0, count = 200) => ({
 test("shows current number of results", async () => {
   const params = getParams();
   getComponent(params);
-  await screen.findByText(`1 - ${params.size} of ${params.count}`);
+  const element = await screen.findByText(
+    `1 - ${params.size} of ${params.count}`
+  );
+  expect(element).toBeInTheDocument();
 });
 
 test("shows current number of results for start index 1", async () => {
   const params = getParams(1);
   getComponent(params);
-  await screen.findByText(`1 - ${params.size} of ${params.count}`);
+  const element = await screen.findByText(
+    `1 - ${params.size} of ${params.count}`
+  );
+  expect(element).toBeInTheDocument();
 });
 
 test("shows `per page` component", async () => {
   getComponent(getParams());
-  await screen.findByText(/show/i);
+  const element = await screen.findByText(/show/i);
+  expect(element).toBeInTheDocument();
 });
 
 test("shows `per page` component for start index 1", async () => {
   getComponent(getParams(1));
-  await screen.findByText(/show/i);
+  const element = await screen.findByText(/show/i);
+  expect(element).toBeInTheDocument();
 });
 
 test("shows last page button on 1st page", () => {
