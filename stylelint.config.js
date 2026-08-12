2a3f08eed56ed0a5d088e8e6e0da58604886cd72
Yujin Wang <yujin.wang@nordcloud.com>
2026-08-12 10:45:24 +0200

    upgrade pat eslint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ["@nordcloud/eslint-config-pat/stylelint.config.js"],
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      rules: {
+        "no-duplicate-selectors": null,
+        "no-descending-specificity": null,
+      },
+    },
+  ],
 };
